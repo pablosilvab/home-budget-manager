dbc4f15c191b50f1ed521c86b620997246e0b2d5
Pablo <pablonicolassilvabravo@gmail.com>
2024-12-06 09:22:49 -0300

    test db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductService } from './products/product.service';
 import { ProductController } from './products/product.controller';
 import { Product } from './products/entities/product.entity';
-import { PriceHistory } from './products/entities/price-history.entity'; 
+import { PriceHistory } from './products/entities/price-history.entity';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, 
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       entities: [Product, PriceHistory],
       synchronize: process.env.TYPEORM_SYNC === 'true',
-      ssl: process.env.NODE_ENV === 'production' ? true : false, // SSL solo en producción
+      ssl: process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false
     }),
     TypeOrmModule.forFeature([Product, PriceHistory]),
   ],
