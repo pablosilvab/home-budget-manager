c71c0572ec4004cce80be059a5f713fd976e9caf
Pablo <pablonicolassilvabravo@gmail.com>
2024-12-08 10:52:48 -0300

    update jest config

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    moduleFileExtensions: [
+      'js',
+      'json',
+      'ts',
+    ],
+    rootDir: 'src',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+      '^.+\\.(t|j)s$': 'ts-jest',
+    },
+collectCoverageFrom: [
+  "**/*.ts",
+  "!**/node_modules/**"
+],
+    coverageDirectory: '../coverage',
+    coverageReporters: ['text', 'lcov'],
+    testEnvironment: 'node',
+  };
+  
