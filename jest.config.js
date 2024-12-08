ccc46c462c0162ec89d6247b7e4ec8581078c0c2
Pablo <pablonicolassilvabravo@gmail.com>
2024-12-08 10:55:31 -0300

    update test stage

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,28 @@
 module.exports = {
     moduleFileExtensions: [
-      'js',
-      'json',
-      'ts',
+        'js',
+        'json',
+        'ts',
     ],
     rootDir: 'src',
     testRegex: '.*\\.spec\\.ts$',
     transform: {
-      '^.+\\.(t|j)s$': 'ts-jest',
+        '^.+\\.(t|j)s$': 'ts-jest',
     },
-collectCoverageFrom: [
-  "**/*.ts",
-  "!**/node_modules/**"
-],
+    collectCoverageFrom: [
+        "**/*.ts",
+        "!**/node_modules/**"
+    ],
     coverageDirectory: '../coverage',
     coverageReporters: ['text', 'lcov'],
     testEnvironment: 'node',
-  };
-  
+
+  coverageThreshold: {
+    global: {
+      statements: 10,
+      branches: 20,
+      functions: 10,
+      lines: 10,
+    },
+  },
+};
