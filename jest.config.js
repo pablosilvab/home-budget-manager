c43594dee03efab5255d6195d729531a5dff63bd
Pablo <pablonicolassilvabravo@gmail.com>
2024-12-09 02:25:52 -0300

    fix sonar config

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,28 @@
 module.exports = {
-    moduleFileExtensions: [
-        'js',
-        'json',
-        'ts',
-    ],
-    rootDir: 'src',
-    testRegex: '.*\\.spec\\.ts$',
-    transform: {
-        '^.+\\.(t|j)s$': 'ts-jest',
-    },
-    collectCoverageFrom: [
-        "**/*.ts",
-        "!**/node_modules/**"
-    ],
-    coverageDirectory: '../coverage',
-    coverageReporters: ['text', 'lcov'],
-    testEnvironment: 'node',
-
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'ts',
+  ],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!**/node_modules/**"
+  ],
+  coverageDirectory: '../coverage',
+  coverageReporters: ['text', 'lcov'],
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: '../coverage',
+      outputName: 'test-report.xml',
+    }],
+  ],
+  testEnvironment: 'node',
   coverageThreshold: {
     global: {
       statements: 10,
